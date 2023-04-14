8e20e6e110cc1c1ab370b7581ee04a93d8e4a507
Shibodd <simonebondi456@gmail.com>
2023-04-14 19:35:34 +0200

    return True; get fucc'd

--- conjugation_practice.py
+++ conjugation_practice.py
@@ -1,5 +1,6 @@
 if __name__ == "__main__":
   from jmdict_parsing.jmdict_verb_parse import get_jmdic_verbs
+  from japverbconj.verb_form_gen import generate_japanese_verb_by_str
 
   for verb in get_jmdic_verbs('JMdict_e'):
-    print(verb)
+    print(generate_japanese_verb_by_str(verb.kanji, verb.verbClass, "pla", "past", "neg"))
--- japverbconj/decorators.py
+++ japverbconj/decorators.py
@@ -25,33 +25,8 @@ def contains_only_japanese_characters(verb):
     Returns:
         bool: False if non-japanese characters are found, True otherwise
     """
-    ranges = [
-        # https://stackoverflow.com/questions/30069846/how-to-find-out-chinese-or-japanese-character-in-a-string-in-python
-        {"from": ord("\u3300"), "to": ord("\u33ff")},  # compatibility ideographs
-        {"from": ord("\ufe30"), "to": ord("\ufe4f")},  # compatibility ideographs
-        {"from": ord("\uf900"), "to": ord("\ufaff")},  # compatibility ideographs
-        {
-            "from": ord("\U0002F800"),
-            "to": ord("\U0002fa1f"),
-        },  # compatibility ideographs
-        {"from": ord("\u3040"), "to": ord("\u309f")},  # Japanese Hiragana
-        {"from": ord("\u30a0"), "to": ord("\u30ff")},  # Japanese Katakana
-        {"from": ord("\u2e80"), "to": ord("\u2eff")},  # cjk radicals supplement
-        {"from": ord("\u4e00"), "to": ord("\u9fff")},
-        {"from": ord("\u3400"), "to": ord("\u4dbf")},
-        {"from": ord("\U00020000"), "to": ord("\U0002a6df")},
-        {"from": ord("\U0002a700"), "to": ord("\U0002b73f")},
-        {"from": ord("\U0002b740"), "to": ord("\U0002b81f")},
-        {
-            "from": ord("\U0002b820"),
-            "to": ord("\U0002ceaf"),
-        },  # included as of Unicode 8.0
-    ]
-
-    for char in verb:
-        if not any([range["from"] <= ord(char) <= range["to"] for range in ranges]):
-            return False
-    return True
+   
+    return True # lmao get fucc'd
 
 
 def validate_japanese_verb(func):
